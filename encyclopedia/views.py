8d1479209104489ef79e72f7d882f7a278ace8e1
Orrv2904 <Omarrv2904@gmail.com>
2023-08-12 21:25:28 -0600

    :lipstick:

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,6 +4,12 @@ from . import util
 
 
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
+    coffe = util.get_entry("coffe")
+    print(entries)
+    print(css_file)
+    print(coffe)
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
