f74921062d7558b6e34cdd3b4537367b6e114e48
Orrv2904 <Omarrv2904@gmail.com>
2023-08-13 00:26:38 -0600

    :construction:

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -84,4 +84,12 @@ def edit(request):
         })
     
 def save_edit(request):
-    return
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = md_to_html(title)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+            })
---
+++ entries/a edit.md
@@ -0,0 +1,5 @@
+# a
+
+
+
+a, this is an edit
