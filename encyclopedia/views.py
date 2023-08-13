fd13bd8e079d3d9a94b90ddb16c307a55bffd886
Orrv2904 <Omarrv2904@gmail.com>
2023-08-12 22:07:49 -0600

    :construction:

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+
+
 
 from . import util
 
-
+ # This view renders the index.html with a list of entries.
+ # It uses the util.list_entries() function to retrieve the list of entries.
 def index(request):
-    entries = util.list_entries()
-    css_file = util.get_entry("CSS")
-    coffe = util.get_entry("coffe")
-    print(entries)
-    print(css_file)
-    print(coffe)
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+# Convert Markdown content to HTML using the provided title.
+# Returns None if no content found, otherwise returns HTML-converted content.
+def md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+    
+# Function call entry
+def entry(request, title):
+    html_content = md_to_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html",{
+            "message": "This entry doesn't exits"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html")
